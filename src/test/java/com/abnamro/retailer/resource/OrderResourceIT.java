33b198d783a9c5c05a7be88433e5e4839d25ba17
hramzy <hramzy@bol.com>
2022-12-24 19:56:45 +0100

    add integration test

---
+++ src/test/java/com/abnamro/retailer/resource/OrderResourceIT.java
@@ -0,0 +1,105 @@
+package com.abnamro.retailer.resource;
+
+import com.abnamro.retailer.entity.Order;
+import com.abnamro.retailer.entity.dto.OrderDTO;
+import com.abnamro.retailer.repository.OrderRepository;
+import static com.abnamro.retailer.util.TestUtils.asJsonString;
+import static com.abnamro.retailer.util.TestUtils.buildRandomOrderDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class OrderResourceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+
+    @Test
+    public void createValidOrder_SavedSuccessfully() throws Exception {
+        OrderDTO orderDTO = buildRandomOrderDTO();
+        MvcResult mvcResult = mockMvc.perform(post("/api/orders")
+                        .content(asJsonString(orderDTO))
+                        .contentType("application/json"))
+                .andExpect(status().isOk()).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        Order order = new ObjectMapper().readValue(json, Order.class);
+        assertOrder(order, orderDTO);
+        Order orderFromDB = orderRepository.findById(order.getId()).orElse(null);
+        assertOrder(orderFromDB, orderFromDB);
+    }
+
+    @Test
+    public void createInvalidOrderMissingData_Error400() throws Exception {
+        OrderDTO orderDTO = buildRandomOrderDTO();
+        orderDTO.setCustomerName(null);
+        mockMvc.perform(post("/api/orders")
+                        .content(asJsonString(orderDTO))
+                        .contentType("application/json"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createInvalidOrderByNotExistProduct_Error400() throws Exception {
+        OrderDTO orderDTO = buildRandomOrderDTO();
+        orderDTO.getProducts().get(0).setProductId(-1L);
+        mockMvc.perform(post("/api/orders")
+                        .content(asJsonString(orderDTO))
+                        .contentType("application/json"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createInvalidOrderByEmptyProducts_Error400() throws Exception {
+        OrderDTO orderDTO = buildRandomOrderDTO();
+        orderDTO.setProducts(List.of());
+        mockMvc.perform(post("/api/orders")
+                        .content(asJsonString(orderDTO))
+                        .contentType("application/json"))
+                .andExpect(status().isBadRequest());
+    }
+
+    private static void assertOrder(Order order, OrderDTO orderDTO) {
+        assertNotNull(order);
+        assertNotNull(orderDTO);
+        assertEquals(order.getCustomerName(), orderDTO.getCustomerName());
+        assertEquals(order.getCustomerEmail(), orderDTO.getCustomerEmail());
+        assertEquals(order.getCustomerPhone(), orderDTO.getCustomerPhone());
+        assertEquals(order.getOrderProducts().size(), orderDTO.getProducts().size());
+        assertEquals(order.getOrderProducts().get(0).getQuantity(), orderDTO.getProducts().get(0).getQuantity());
+        assertEquals(order.getOrderProducts().get(0).getProduct().getId(), orderDTO.getProducts().get(0).getProductId());
+    }
+
+    private static void assertOrder(Order order1, Order order2) {
+        assertNotNull(order1);
+        assertNotNull(order2);
+        assertEquals(order1.getId(), order2.getId());
+        assertEquals(order1.getCustomerName(), order2.getCustomerName());
+        assertEquals(order1.getCustomerEmail(), order2.getCustomerEmail());
+        assertEquals(order1.getCustomerPhone(), order2.getCustomerPhone());
+        assertEquals(order1.getTotalPrice(), order2.getTotalPrice());
+        assertEquals(order1.getOrderProducts().size(), order2.getOrderProducts().size());
+        assertEquals(order1.getOrderProducts().get(0).getId(), order2.getOrderProducts().get(0).getId());
+    }
+
+}
--- src/test/java/com/abnamro/retailer/util/TestUtils.java
+++ src/test/java/com/abnamro/retailer/util/TestUtils.java
@@ -1,11 +1,19 @@
 package com.abnamro.retailer.util;
 
+import com.abnamro.retailer.entity.Order;
+import com.abnamro.retailer.entity.OrderProduct;
 import com.abnamro.retailer.entity.Product;
+import com.abnamro.retailer.entity.dto.OrderDTO;
+import com.abnamro.retailer.entity.dto.OrderProductDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class TestUtils {
 
@@ -23,7 +31,40 @@ public class TestUtils {
                 .id(RandomUtils.nextLong())
                 .name(RandomStringUtils.randomAlphabetic(200))
                 .description(RandomStringUtils.randomAlphabetic(1000))
-                .price(BigDecimal.valueOf(RandomUtils.nextDouble()))
+                .price(buildRandomBigDecimal())
+                .build();
+    }
+
+    public static Order buildRandomOrder() {
+        List<OrderProduct> orderProducts = IntStream.range(1, RandomUtils.nextInt(1, 6))
+                .mapToObj(i -> buildRandomOrderProduct())
+                .collect(Collectors.toList());
+
+        return Order.builder()
+                .id(RandomUtils.nextLong())
+                .customerName(buildRandomEmail())
+                .customerPhone(buildNumberMobileNumber())
+                .orderProducts(orderProducts)
+                .build();
+    }
+
+    public static OrderDTO buildRandomOrderDTO() {
+        List<OrderProductDTO> orderProductDTOS = LongStream.range(1, RandomUtils.nextInt(2, 5))
+                .mapToObj(TestUtils::buildRandomOrderProductDTO)
+                .collect(Collectors.toList());
+
+        return OrderDTO.builder()
+                .customerEmail(buildRandomEmail())
+                .customerName(RandomStringUtils.randomAlphabetic(200))
+                .customerPhone(buildNumberMobileNumber())
+                .products(orderProductDTOS)
+                .build();
+    }
+
+    public static OrderProductDTO buildRandomOrderProductDTO(Long productId) {
+        return OrderProductDTO.builder()
+                .productId(productId)
+                .quantity(RandomUtils.nextInt())
                 .build();
     }
 
@@ -34,5 +75,24 @@ public class TestUtils {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static OrderProduct buildRandomOrderProduct() {
+        return OrderProduct.builder()
+                .quantityPrice(buildRandomBigDecimal())
+                .quantity(RandomUtils.nextInt())
+                .build();
+    }
+
+    private static BigDecimal buildRandomBigDecimal() {
+        return BigDecimal.valueOf(RandomUtils.nextDouble());
+    }
+
+    private static String buildRandomEmail() {
+        return RandomStringUtils.randomAlphabetic(12) + "@gmail.com";
+    }
+
+    private static String buildNumberMobileNumber() {
+        return "+31" + RandomStringUtils.randomNumeric(10);
     }
 }
