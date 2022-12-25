84719cd7e772c9bc999b459d9c5447333288ea61
hramzy <hramzy@bol.com>
2022-12-25 15:36:01 +0100

    Add lowQuantity indicator to the report endpoints

--- src/main/java/com/abnamro/retailer/resource/ReportResource.java
+++ src/main/java/com/abnamro/retailer/resource/ReportResource.java
@@ -3,6 +3,8 @@ package com.abnamro.retailer.resource;
 import com.abnamro.retailer.entity.Order;
 import com.abnamro.retailer.entity.Product;
 import com.abnamro.retailer.entity.SaleAmountPerDay;
+import com.abnamro.retailer.entity.dto.ReportProductDTO;
+import com.abnamro.retailer.mapper.ProductMapper;
 import com.abnamro.retailer.service.ReportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/reports")
@@ -32,19 +34,24 @@ public class ReportResource {
     }
 
     @GetMapping("products/top-selling/today")
-    public ResponseEntity<List<Product>> findTopSellingProductsOfToday() {
+    public ResponseEntity<List<ReportProductDTO>> findTopSellingProductsOfToday() {
         List<Product> products = reportService.findTopSellingProductsOfToday();
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(products.stream()
+                .map(ProductMapper.INSTANCE::mapToReportProductDTO)
+                .collect(Collectors.toList()));
     }
 
     @GetMapping("products/least-selling/month")
-    public ResponseEntity<List<Product>> findLeastSellingProductsOfMonth() {
+    public ResponseEntity<List<ReportProductDTO>> findLeastSellingProductsOfMonth() {
         List<Product> products = reportService.findLeastSellingProductsOfMonth();
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(products.stream()
+                .map(ProductMapper.INSTANCE::mapToReportProductDTO)
+                .collect(Collectors.toList()));
     }
 
     @GetMapping("orders/count-sale")
-    public ResponseEntity<List<SaleAmountPerDay>> calculateSaleAmountWithinTime(@RequestParam Date fromDate, @RequestParam Date toDate) {
+    public ResponseEntity<List<SaleAmountPerDay>> calculateSaleAmountWithinTime(@RequestParam Date fromDate,
+                                                                                @RequestParam Date toDate) {
         List<SaleAmountPerDay> totalAmount = reportService.calculateSaleAmountWithinTime(fromDate, toDate);
         return ResponseEntity.ok(totalAmount);
     }
