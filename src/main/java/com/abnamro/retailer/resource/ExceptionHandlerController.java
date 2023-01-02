eb422fdd91fbdce403a5945dfb87ca1c80ebcc3b
hitham ramzy <hitham.ramzy@gmail.com>
2023-01-02 17:16:56 +0100

    Secure the endpoints by basic auth, and update readme file

--- src/main/java/com/abnamro/retailer/resource/ExceptionHandlerController.java
+++ src/main/java/com/abnamro/retailer/resource/ExceptionHandlerController.java
@@ -6,28 +6,54 @@ import static com.abnamro.retailer.util.ErrorConstants.GENERAL_ERROR;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * The type Exception handler controller.
+ */
 @ControllerAdvice()
 public class ExceptionHandlerController {
 
+    /**
+     * The Logger.
+     */
     Logger logger = LoggerFactory.getLogger(ExceptionHandlerController.class);
 
+    /**
+     * Handle invalid input exception response entity.
+     *
+     * @param e the e
+     * @return the response entity
+     */
     @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<String> handleInvalidInputException(InvalidInputException e) {
         logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
+    /**
+     * Handle not found exception response entity.
+     *
+     * @param e the e
+     * @return the response entity
+     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
         logger.error(e.getMessage());
         return ResponseEntity.notFound().build();
     }
 
+    /**
+     * Handle method argument not valid exception response entity.
+     *
+     * @param e the e
+     * @return the response entity
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         logger.error(e.getMessage());
@@ -37,6 +63,24 @@ public class ExceptionHandlerController {
         return ResponseEntity.badRequest().body(message);
     }
 
+    /**
+     * Handle access denied exception response entity.
+     *
+     * @param e the e
+     * @return the response entity
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+        logger.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+    /**
+     * Handle exception response entity.
+     *
+     * @param e the e
+     * @return the response entity
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         logger.error(e.getMessage());
--- src/main/java/com/abnamro/retailer/resource/OrderResource.java
+++ src/main/java/com/abnamro/retailer/resource/OrderResource.java
@@ -3,7 +3,10 @@ package com.abnamro.retailer.resource;
 import com.abnamro.retailer.entity.Order;
 import com.abnamro.retailer.entity.dto.OrderDTO;
 import com.abnamro.retailer.service.OrderService;
+import static com.abnamro.retailer.util.SecurityUtils.ROLE_ADMIN;
+import static com.abnamro.retailer.util.SecurityUtils.ROLE_USER;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,23 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * The type Order resource.
+ */
 @RestController
 @RequestMapping("/api/orders")
 public class OrderResource {
 
     private final OrderService orderService;
 
+    /**
+     * Instantiates a new Order resource.
+     *
+     * @param orderService the order service
+     */
     public OrderResource(OrderService orderService) {
         this.orderService = orderService;
     }
 
+    /**
+     * Gets all.
+     *
+     * @return the all
+     */
     @GetMapping
+    @Secured({ROLE_ADMIN})
     public ResponseEntity<List<Order>> getAll() {
         List<Order> orders = orderService.findAll();
         return ResponseEntity.ok(orders);
     }
 
+    /**
+     * Gets one by id.
+     *
+     * @param id the id
+     * @return the one by id
+     */
     @GetMapping("{id}")
+    @Secured({ROLE_ADMIN})
     public ResponseEntity<Order> getOneById(@PathVariable Long id) {
         Order order = orderService.findById(id);
         if (order == null) {
@@ -39,7 +63,14 @@ public class OrderResource {
         return ResponseEntity.ok(order);
     }
 
+    /**
+     * Create response entity.
+     *
+     * @param orderDTO the order dto
+     * @return the response entity
+     */
     @PostMapping
+    @Secured({ROLE_ADMIN, ROLE_USER})
     public ResponseEntity<Order> create(@RequestBody @Valid OrderDTO orderDTO) {
         Order savedOrder = orderService.save(orderDTO);
         return ResponseEntity.ok(savedOrder);
